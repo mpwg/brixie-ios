8ca452b40b97dd42cb9b0c8906fa85b0876a6dfc
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-08-26 14:03:58 +0200

    fix

---
+++ Brixie/BrixieTests/BrixieTests.swift
@@ -0,0 +1,16 @@
+//
+//  BrixieTests.swift
+//  BrixieTests
+//
+//  Created by Matthias Wallner-Géhri on 26.08.25.
+//
+
+import Testing
+
+struct BrixieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
